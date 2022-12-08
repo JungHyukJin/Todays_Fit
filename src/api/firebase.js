59a9bf59898bc00c94e2012480fdee7fdb5de135
JungHyukJin <hyukjin4976@naver.com>
2022-12-08 18:18:00 +0900

    로그인, 어드민 사용자 구현

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,4 +1,12 @@
 import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { getDatabase, ref, child, get } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -7,4 +15,62 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
+//firebase 초기화
 const app = initializeApp(firebaseConfig);
+// Google 공급자 개체를 사용하여 Firebase에 인증합니다.
+// 팝업 창을 열거나 로그인 페이지로 리디렉션하여 사용자에게 Google 계정으로
+// 로그인하도록 요청할 수 있습니다. 리디렉션 방법은 모바일 장치에서 선호됩니다
+const auth = getAuth();
+// google 공급자 개체의 인스턴스 만들기
+const provider = new GoogleAuthProvider();
+// firebase 실시간 데이터베이스 사용, admin관련 작업 전에 필요
+const database = getDatabase(app);
+
+// 로그인
+// export async function login() {
+//   return signInWithPopup(auth, provider)
+//     .then((result) => {
+//       const user = result.user;
+//       return user; // 로그인 시 사용자 객체 리턴
+//     })
+//     .catch((error) => {
+//       console.error("error", error);
+//     });
+// }
+export function login() {
+  signInWithPopup(auth, provider);
+}
+
+// 로그아웃
+// export async function logout() {
+//   const auth = getAuth();
+//   return signOut(auth).then(() => {
+//     return null;
+//   });
+// }
+export function logout() {
+  signOut(auth);
+}
+
+// 인증 상태 관찰자 설정 및 사용자 데이터 가져오기
+export function onUserStateChange(callback) {
+  const auth = getAuth();
+  onAuthStateChanged(auth, async (user) => {
+    const updatedUser = user ? await adminUser(user) : null;
+    callback(updatedUser);
+  });
+}
+
+// firebase db에 있는 데이터 한번 읽어오기
+async function adminUser(user) {
+  return get(ref(database, "admins")) //
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        // console.log('1', snapshot.val());
+        const admins = snapshot.val();
+        const isAdmin = admins.includes(user.uid);
+        return { ...user, isAdmin };
+      }
+      return user;
+    });
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { GrCoatCheck } from "react-icons/gr";
 import { BsPencilSquare } from "react-icons/bs";
+import { login, logout, onUserStateChange } from "../api/firebase.js";
+import User from "./User.jsx";
 
 export default function Navbar() {
+  const [user, setUser] = useState();
+
+  // 사용자 정보가 남아있다면 로그인 유지
+  useEffect(() =>{
+    onUserStateChange(user => {
+      console.log(user)
+      setUser(user);
+    });
+  }, [])
+
+  // const loginHandler = () => {
+  //   login().then((user) => setUser(user));
+  // };
+
+  // const logoutHandler = () => {
+  //   logout().then((user) => setUser(user));
+  // };
+
   return (
-    <header className='flex justify-between border-b border-gray-300 p-4'>
-      <Link to="/" className='flex items-center text-4xl text-brand gap-2'>
-        <GrCoatCheck className='text-red'/>
+    <header className="flex justify-between border-b border-gray-300 p-4">
+      <Link to="/" className="flex items-center text-4xl text-brand gap-2">
+        <GrCoatCheck className="text-red" />
         <p>Today's Fit</p>
       </Link>
-      <nav className='flex items-center gap-4 font-semibold'>
+      <nav className="flex items-center gap-4 font-semibold">
         <Link to="/products">Products</Link>
         <Link to="/cart">Cart</Link>
         <Link to="/products/new">
-          <BsPencilSquare className='text-2xl'/>
+          <BsPencilSquare className="text-2xl" />
         </Link>
-        <button>Login</button>
+        {user && <User user={user} />}
+        {!user && <button onClick={login}>Login</button>}
+        {user && <button onClick={logout}>Logout</button>}
       </nav>
     </header>
   );
